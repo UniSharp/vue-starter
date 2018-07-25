80cd7006e4a81810bd6f8f2d3a46a20324678e36
Sasaya <storyn26383@gmail.com>
2018-07-25 23:09:48 +0800

    fix font awesome plugin

--- template/plugins/FontAwesome.js
+++ template/plugins/FontAwesome.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+{{# travis }}
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+{{ else }}
 import { fal } from '@fortawesome/pro-light-svg-icons'
 import { fas } from '@fortawesome/pro-solid-svg-icons'
 import { far } from '@fortawesome/pro-regular-svg-icons'
+{{/ travis }}
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+{{# travis }}
+library.add(fab, fas, far)
+{{ else }}
 library.add(fab, fal, fas, far)
+{{/ travis }}
 
 Vue.mixin({
   components: { fa: FontAwesomeIcon }
