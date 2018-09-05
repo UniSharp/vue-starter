e9dede0f0c1b14a8c8179b6c3ad014faddcd46ee
Youchen Lee (isacl) <deploy@travis-ci.org>
2018-09-05 04:13:01 +0000

    Deploy UniSharp/vue-starter to github.com/UniSharp/vue-starter.git:gh-pages

---
+++ _nuxt/pages/auth/logout.3a1270ec1f61c10aa25a.js
@@ -0,0 +1,1 @@
+webpackJsonp([4],{ahNB:function(e,t,a){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var o=a("lbHh"),n=a.n(o);t.default={asyncData:function(e){var t=e.store,a=e.redirect;n.a.remove("api_token"),t.commit("auth/logout"),a("/")}}}});
